15fe11207fe73b24d20b97eca10a0cccf0699d0a
gopaljilab <gopaljilab@gmail.com>
2023-08-29 22:16:25 +0530

    add comment

--- src/Prime_number.java
+++ src/Prime_number.java
@@ -7,16 +7,22 @@ public class Prime_number {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number : ");
         int num = sc.nextInt();
+        // 0 and 1 are not prime numbers
+        // change flag to 1 for non - prime number
         if (num == 0 || num == 1) {
             flag = 1;
         }
+            // a number is divisible by only half itself
             for ( i=2 ; i <= num /2 ; i++)
             {
+                // if the number divisible by i , then number is not prime
+                // change the flag to 1 for non - prime number
                 if (num % i == 0) {
                     flag = 1;
                     break;
                 }
             }
+            // flag is 0 for prime number
         if (flag == 0)
         {
             System.out.println(num + " is a Prime Number. ");
