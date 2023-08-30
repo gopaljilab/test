80765cde4dada6f255c1402d0ad74fa06be565a7
gopaljilab <gopaljilab@gmail.com>
2023-08-30 23:11:45 +0530

    Commit PrimeNumber

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -3,33 +3,34 @@ import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args)
     {
-        int i,flag = 0;
+        int i;
+        boolean isPrime = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number : ");
         int num = sc.nextInt();
         // 0 and 1 are not prime numbers
         // change flag to 1 for non - prime number
         if (num == 0 || num == 1) {
-            flag = 1;
+            isPrime = false;
         }
             // a number is divisible by only half itself
-            for ( i=2 ; i <= num /2 ; i++)
+            for (i = 2; i * i < num; i++)
             {
                 // if the number divisible by i , then number is not prime
                 // change the flag to 1 for non - prime number
                 if (num % i == 0) {
-                    flag = 1;
+                    isPrime = false;
                     break;
                 }
             }
             // flag is 0 for prime number
-        if (flag == 0)
+        if (isPrime)
         {
-            System.out.println(num + " is a Prime Number. ");
+            System.out.println(num + " is a Prime Number.");
         }
         else
         {
-            System.out.println(num + " is Not a Prime Number. ");
+            System.out.println(num + " is Not a Prime Number.");
         }
     }
 }
