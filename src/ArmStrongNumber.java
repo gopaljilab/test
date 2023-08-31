96b33f0b37801fe882e387c4cb29ff43c213e049
gopaljilab <gopaljilab@gmail.com>
2023-08-31 21:49:55 +0530

    Reformat code

--- src/ArmStrongNumber.java
+++ src/ArmStrongNumber.java
@@ -10,16 +10,16 @@ public class ArmStrongNumber {
         while(temp != 0)   // 153 != 0 --> 15 != 0 --> 1!=0 --> 0==0 break
         {
             remainder = temp % 10;    // 3 --> 5 --> 1
-            sum = sum + remainder*remainder*remainder ;  // 0 + 3*3*3 = 27 --> 27 + 5*5*5 = 152 --> 152+1*1*1 = 153
-            temp = temp/10; // 153/10 = 15 --> 1 --> 0
+            sum = sum + remainder * remainder * remainder ;  // 0 + 3*3*3 = 27 --> 27 + 5*5*5 = 152 --> 152+1*1*1 = 153
+            temp = temp / 10; // 153/10 = 15 --> 1 --> 0
         }
         if (sum == num)     // check the Sum Equal to Number or not ------> 153 == 153 
         {
-            System.out.println(num+ " is An Armstrong Number. ");
+            System.out.println(num + " is An Armstrong Number.");
         }
         else
         {
-            System.out.println(num+ " is Not An Armstrong Number. ");
+            System.out.println(num + " is Not An Armstrong Number.");
         }
     }
 }
